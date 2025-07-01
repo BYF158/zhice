3a1f7eddd7010f230163b2598115df39f6728d7b
BYF158 <2357873368@qq.com>
2025-07-01 15:58:47 +0800

    第一次提交

---
+++ zhice-admin/src/main/java/com/etsong/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.etsong.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.etsong.common.core.controller.BaseController;
+import com.etsong.common.core.domain.AjaxResult;
+import com.etsong.common.core.domain.model.RegisterBody;
+import com.etsong.common.utils.StringUtils;
+import com.etsong.framework.web.service.SysRegisterService;
+import com.etsong.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
---
+++ zhice-common/src/main/java/com/etsong/common/enums/BusinessStatus.java
@@ -0,0 +1,20 @@
+package com.etsong.common.enums;
+
+/**
+ * 操作状态
+ * 
+ * @author ruoyi
+ *
+ */
+public enum BusinessStatus
+{
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
